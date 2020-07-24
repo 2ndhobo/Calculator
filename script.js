10ca640650a794b5a79053f844cb87cba241ea82
2ndhobo <67642846+2ndhobo@users.noreply.github.com>
2020-07-24 19:07:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,13 +13,7 @@ const updateScreen=(number)=>{
   calculatorScreen.value=number
 }
 
-const numbers=document.querySelectorAll(".number")
 
-numbers.forEach((number)=>{
-  number.addEventListener("click",(event)=>{
-	updateScreen(event.target.value)
-  })
-})
 
 let prevNumber=''
 let calculationOperator=''
